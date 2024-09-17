47f91ee5cdd214a7456eec1fbb2398bb65746328
Dana Mulyana <danabontot@gmail.com>
2024-09-17 20:12:26 +0700

    #project# - fixing docs

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/api/auth', authRoutes);
 
 app.use(express.static(path.join(__dirname, 'docs')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 // Connect to MongoDB
