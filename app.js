6308cf362b981515182b176d756f7e9a271c6ac3
Dana Mulyana <danabontot@gmail.com>
2024-09-18 13:53:54 +0700

    #project# - adding cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //cors
-const allowedOrigins = process.env.CORS_ORIGIN.split(',');
+const allowedOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : [];
 
 const corsOptions = {
   origin: function (origin, callback) {
