869cc60587404f2119d289afdb6f29f7594bb3cb
Dana Mulyana <danabontot@gmail.com>
2024-09-18 14:11:41 +0700

    #project# - adding cors

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 //cors
 const allowedOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : [];
 
